df97ab599f04972d6b703f268fc15bc508d16eac
Hojat Joshani <h.joshani@gmail.com>
2024-10-04 10:56:58 +0330

    sidebar

--- src/app/dashboardWrapper.tsx
+++ src/app/dashboardWrapper.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import Navbar from "./(components)/Navbar";
+import Navbar from "@/app/(components)/Navbar";
+import Sidebar from "@/app/(components)/Sidebar"
 
 const dashboardWrapper = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex min-h-screen w-full bg-gray-50 text-gray-900">
-      {/* Sidebar */}
-      نوار کناری
+      <Sidebar />
       <main
         className={`flex w-full flex-col bg-gray-50 dark:bg-dark-bg md:pr-64`}
       >
