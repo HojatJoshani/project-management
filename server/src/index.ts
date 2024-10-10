39a3260c5c3d9b9e9442e61d9235c02f06bd69d2
Hojat Joshani <h.joshani@gmail.com>
2024-10-10 12:00:00 +0330

    update

--- server/src/index.ts
+++ server/src/index.ts
@@ -24,4 +24,7 @@ app.get("/", (req, res) => {
 });
 
 /* SERVER */
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`سرور روی پورت ${port} فعال است.`)
+})
